9e08e8f1ee8f58491b01a60d2350b642a8a52066
Santhoshavs <santhoshavs9823@gmail.com>
2023-11-07 00:28:22 +0530

    task completed

--- script.js
+++ script.js
@@ -25,18 +25,18 @@ request.onload = function(){
 
 
 //!Print all the country names,regions,subregions and population..
-var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all",true)
-request.send();
-request.onload = function(){
-    var data = request.response;
-    var result = JSON.parse(data);
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].name.common);}
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].region);}
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].subregion);} 
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].population);}   
+var request1 = new XMLHttpRequest();
+request1.open("GET","https://restcountries.com/v3.1/all",true)
+request1.send();
+request1.onload = function(){
+    var data = request1.response;
+    var result1 = JSON.parse(data);
+    for(var i=0;i<result1.length;i++){
+        console.log(result1[i].name.common);}
+    for(var i=0;i<result1.length;i++){
+        console.log(result1[i].region);}
+    for(var i=0;i<result1.length;i++){
+        console.log(result1[i].subregion);} 
+    for(var i=0;i<result1.length;i++){
+        console.log(result1[i].population);}   
 }
